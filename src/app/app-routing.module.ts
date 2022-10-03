a83dc112f188a001e6962c7f4ed36af7bb818a56
shamil-t <tshamil90@gmail.com>
2022-10-03 14:47:16 +0530

    addedd password vault

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotesComponent } from './notes/notes.component';
+import { ReminderComponent } from './reminder/reminder.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { VaultComponent } from './vault/vault.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'home/tasks', component: TasksComponent },
-  { path: 'home/notes', component: NotesComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home/tasks',
+    component: TasksComponent,
+  },
+  {
+    path: 'home/notes',
+    component: NotesComponent,
+  },
+  {
+    path: 'home/reminder',
+    component: ReminderComponent,
+  },
+  {
+    path: 'home/vault',
+    component: VaultComponent,
+  },
 ];
 
 @NgModule({
