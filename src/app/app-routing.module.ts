9ae4cc23bb8fe0f429735bfca77e491a8eb255f5
shamil-t <tshamil90@gmail.com>
2022-10-04 19:18:27 +0530

    Added User info Edit Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
     path: 'home/vault',
     component: VaultComponent,
   },
+  { path: '*', redirectTo: 'home' },
 ];
 
 @NgModule({
---
+++ src/app/types/user.type.ts
@@ -0,0 +1,10 @@
+export interface User {
+    name:string;
+    email:string;
+    empID:number;
+    designation:string;
+    manager :{
+        name:string;
+        email:string;
+    }
+}
