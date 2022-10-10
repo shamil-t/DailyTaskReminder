cd02a6ea83c836054edce506d27bf3c93852c453
shamil-t <tshamil90@gmail.com>
2022-10-11 00:56:10 +0530

    login update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../types/login.type';
+import { Register } from '../types/register.type';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  signUp: boolean = false;
+
+  register: Register = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: ''
+  }
+
+  login: Login = {
+    email: '',
+    password: ''
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSignUp() {
+    this.signUp = !this.signUp
+  }
+
+  onSignInSignUp() {
+    if (this.signUp) {
+      this.register.email = this.login.email
+      this.register.password = this.login.password
+      console.log(this.register);
+    }
+
+    else
+      console.log(this.login);
   }
 
 }
