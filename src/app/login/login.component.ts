76ef2859016b012101291bcbe5f9216cda013efb
shamil-t <tshamil90@gmail.com>
2022-10-11 21:51:02 +0530

    updated login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertComponent } from 'src/utils/alert/alert.component';
+import { AuthService } from '../services/auth.service';
 import { Login } from '../types/login.type';
 import { Register } from '../types/register.type';
+import { UserResponse } from '../types/user_response.type';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +12,8 @@ import { Register } from '../types/register.type';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild(AlertComponent)
+  toast: AlertComponent = new AlertComponent()
 
   signUp: boolean = false;
 
@@ -23,8 +29,10 @@ export class LoginComponent implements OnInit {
     password: ''
   }
 
+  toastText: string = ''
 
-  constructor() { }
+
+  constructor(private as: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -39,9 +47,24 @@ export class LoginComponent implements OnInit {
       this.register.password = this.login.password
       console.log(this.register);
     }
-
-    else
+    else {
       console.log(this.login);
+      this.as.login(this.login).subscribe((r: any) => {
+        if (r.hasOwnProperty('password')) {
+          this.toastText = 'Incorrect Password'
+          this.toast.show()
+        }
+        if (r.hasOwnProperty('email')) {
+          this.toastText = 'User not Registered Please Sign In'
+          this.toast.show()
+        }
+        if (r.hasOwnProperty('user')) {
+          localStorage.setItem('user', JSON.stringify(r.user))
+          this.router.navigate(['home'])
+        }
+      })
+    }
+
   }
 
 }
